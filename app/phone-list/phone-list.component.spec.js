951b6bdb5ecd5b80d384d06ed0c2a480840e1bcb
Kocsis Ferenc <kocsis.ferenc@schibsted.hu>
2016-11-22 15:09:25 +0100

    Two-way Data Binding.
    Changes:
     - Component Template
     - Component Controller
     - Testing

--- app/phone-list/phone-list.component.spec.js
+++ app/phone-list/phone-list.component.spec.js
@@ -1,17 +1,24 @@
 describe('phoneList', function() {
 
-		// Load the module that contains the `phoneList` component before each test
-		beforeEach(module('phoneList'));
+	// Load the module that contains the `phoneList` component before each test
+	beforeEach(module('phoneList'));
 
-		// Test the controller
-		describe('PhoneListController', function() {
+	// Test the controller
+	describe('PhoneListController', function() {
+		var ctrl;
 
-			it('should create a `phones` model with 3 phones', inject(function($componentController) {
-				var ctrl = $componentController('phoneList');
+		beforeEach(inject(function($componentController) {
+			ctrl = $componentController('phoneList');
+		}));
 
-				expect(ctrl.phones.length).toBe(3);
-			}));
-
+		it('should create a `phones` model with 3 phones', function() {
+			expect(ctrl.phones.length).toBe(3);
 		});
+
+		it('should set a default value for the `orderProp` model', function() {
+			expect(ctrl.orderProp).toBe('age');
+		});
+
+	});
 
 });
